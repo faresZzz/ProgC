be2c106ab738f9901ea3b67719e4bf7b6f293361
Maxime Curral <maxime.curral@cpe.fr>
2021-10-12 16:54:21 +0200

    Commentaire operateur.c (TP1)

--- TP1/operateur.c
+++ TP1/operateur.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(){
+
+    //Initialisation des variables
     float a;
     a=16;
 
@@ -13,6 +15,7 @@ int main(){
     int d;
     d=3;
 
+    //Affichage des résultats des opérations
     printf("16+3=%f\n", a+b);
     printf("16-3=%f\n", a-b);
     printf("16*3=%f\n", a*b);
