5a3c6ede80f251ba2d6afdebd59a7fc97f95692f
Maxime Curral <maxime.curral@cpe.fr>
2021-10-12 20:27:27 +0200

    Commentaires bits.c (TP2)

--- TP2/bits.c
+++ TP2/bits.c
@@ -3,13 +3,16 @@
 
 int main()
 {
+    //On choisit le nombre à "analyser"
     int chiffre = 1572847; //2097151 
     int vingt;
     int quatre;
     
+    //Opérateurs de décalage de 20 bits et de 4 bits
     vingt = chiffre >>20;
     quatre = chiffre >>4;
     
+    //On teste si les 4° et 20° bits de *chiffre* sont bien à 1 et on affiche le résultat
     if (vingt%2 == 1 && quatre % 2 == 1)
     {
         printf("Le 20° et le 4° bit du chiffre(%d) sont à 1",chiffre);
