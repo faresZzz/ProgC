eb3e384cfb6dde3327f9c41492597ae5134a8447
Maxime Curral <maxime.curral@cpe.fr>
2021-10-12 20:46:01 +0200

    Commentaires etudiant2.c (TP2)

--- TP2/etudiant2.c
+++ TP2/etudiant2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//On déclare les éléments à afficher
 typedef struct etudiant{
     char nom[10];
     char prenom[30];
@@ -11,6 +12,7 @@ typedef struct etudiant{
 
 int main()
 {
+    //On déclare les nom, prénom, ville et notes de chaque étudiant
     char nom[5][10] ={"Pierre", "Paul", "Jack", "Moi", "Hugo"};
     char prenom[5][30] ={"Quiroule", "Enta", "Adit", "Sissure", "le"};
     char ville[5][30] ={"Paris", "Lyon", "Marseille", "Lille", "Gaule"};
@@ -19,6 +21,7 @@ int main()
     
     etudiant liste_etudiant[5];
     
+    //Pour chaque étudiant , on remplit liste_etudiant avec les bonnes informations
     for (int i = 0; i < 5; i++)
     {
         strcpy(liste_etudiant[i].nom, nom[i]);
@@ -27,6 +30,8 @@ int main()
         liste_etudiant[i].progC = progC[i];
         liste_etudiant[i].systeme = systeme[i];
     }
+
+    //On affiche les phrases descriptives des étudiants grâce à struct
     for (int i = 0; i < 5; i++)
     {
         printf("Bonjour je suis %s %s, j\'habite à %s, mes notes en Programmation en C et Système d'exploitation sont: %d, %d\n", liste_etudiant[i].nom, liste_etudiant[i].prenom, liste_etudiant[i].ville, liste_etudiant[i].progC, liste_etudiant[i].systeme);
