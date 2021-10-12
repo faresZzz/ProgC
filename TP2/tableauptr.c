e39b2355ad1c8dd4c1aa8ef41a040bca72cb84d8
Maxime Curral <maxime.curral@cpe.fr>
2021-10-12 20:54:15 +0200

    Commentaires tableauptr.c (TP2)

--- TP2/tableauptr.c
+++ TP2/tableauptr.c
@@ -8,21 +8,22 @@
 
 int main()
 {
+    //On crée les tableaux et les pointeurs correspondants
     int tab_int[10];
     int *ptr_int = tab_int;
     
     float tab_float[10];
     float *ptr_float = tab_float;
   
-    
+    //On remplit les tableaux aléatoirement
     srand(time(NULL));
-    
     for (int i = 0; i < 10; i++)
     {
         *(ptr_int + i) = rand()%100;
         *(ptr_float + i) = (rand()%100);
     }
     
+    //On parcourt les indices et à chaque fois qu'il y en un pair, on multiplie par 3 la valeur qu'il pointe
     for (int i = 0; i < 10; i++)
     {
         if ((i % 2) == 0)
@@ -31,6 +32,8 @@ int main()
             *(ptr_float + i) *= 3;
         }
     }
+
+    //On affiche les tableaux
     printf("int\tfloat\n");
     for (int i = 0; i < 10; i++)
     {
