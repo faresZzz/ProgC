62eb89e6597597c488a4f8d9089053d87af926ab
Maxime Curral <maxime.curral@cpe.fr>
2021-10-12 20:40:50 +0200

    Commentaires etudiants.c (TP2)

--- TP2/etudiant.c
+++ TP2/etudiant.c
@@ -3,12 +3,14 @@
 
 int main()
 {
+    //On déclare les nom, prénom, ville et notes de chaque étudiant
     char nom[5][10] ={"Pierre", "Paul", "Jack", "Moi", "Hugo"};
     char prenom[5][30] ={"Quiroule", "Enta", "Adit", "Sissure", "Le"};
     char ville[5][30] ={"Paris", "Lyon", "Marseille", "Lille", "Gaule"};
     int progC[5] ={10, 11, 12, 13, 14};
     int systeme[5] ={10, 9, 8, 7, 6};
     
+    //Pour chaque étudiant, on affiche une phrase de description
     for (int i = 0; i < 5; i++)
     {
         printf("Bonjour je suis %s %s, j\'habite à %s, mes notes en Programmation en C et Système d'exploitation sont: %d, %d\n", nom[i], prenom[i], ville[i], progC[i], systeme[i]);
