1dc4896785108eebefcdd289b70fc75bf34d1ec1
Maxime Curral <maxime.curral@cpe.fr>
2021-10-12 16:52:00 +0200

    Ajout sizeof_types.c (TP1)

---
+++ TP1/sizeof_types.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("taille de char : %li\n",sizeof(char));
+    printf("taille de short : %li\n",sizeof(short)); 
+    printf("taille de int : %li\n",sizeof(int));
+    printf("taille de long int : %li\n",sizeof(long int));
+    printf("taille de long long int : %li\n",sizeof(long long int));
+    printf("taille de float : %li\n",sizeof(float));
+    printf("taille de double : %li\n",sizeof(double));
+    printf("taille de long double : %li\n",sizeof(long double));
+}
